6f4f5801d7a0461201e47bc0d14778ec25321416
taiga02320091-lab <taiga02320091@gmail.com>
2026-09-09 23:11:16 +0900

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,15 @@
+self.addEventListener("install", () => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("notificationclick", event => {
+  event.notification.close();
+
+  event.waitUntil(
+    clients.openWindow("./")
+  );
+});
